2e49c3056168730edbdafa5e4362f351fc0d62e4
Devashish Chakraborty <devashishchakra@gmail.com>
2022-12-28 22:51:06 +0530

    Added createGrid Function

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
+function createGrid(axis) {
+    gridContainer.textContent = '';
+    for (let i = 0; i < axis; i++) {
+        for (let j = 0; j < axis; j++) {
+            const gridItem = document.createElement('div');
+            gridItem.classList.add('grid-item');
+            gridContainer.appendChild(gridItem);
+        }
+    }
+    gridContainer.setAttribute('style',
+        `grid-template-rows: repeat(${axis}, 1fr); 
+        grid-template-columns: repeat(${axis}, 1fr);`);
+
+}
+
 let squares = 16;
 const gridContainer = document.querySelector('.grid-container');
+
